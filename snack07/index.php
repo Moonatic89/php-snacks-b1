f21f8401ae1b7e6e41bd0a8c55e001190c8e02e4
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-02 15:57:42 +0100

    Snack 07 - Students array

--- snack07/index.php
+++ snack07/index.php
@@ -1,3 +1,50 @@
+<?php
+ 
+//Crea un array di alunni, ognun contenente Nome, Cognome ed un array di voti. Stampa Nome, Cognome e media dei voti.
+
+    $class = [
+            
+             [
+                'name' => 'Stefano',
+                'lastname' => 'Nesi',
+                'vote' => [7,8,7,9,9,8,6,10,8,6]
+     
+             ],
+
+              [
+                'name' => 'Andrea',
+                'lastname' => 'Tosi',
+                'vote' => [8,6,7,6,9,9,6,10,10,6]
+     
+             ],
+
+              [
+                'name' => 'Matteo',
+                'lastname' => 'Fregonese',
+                'vote' => [7,10,7,9,9,8,6,10,8,6]
+     
+             ],
+
+              [
+                'name' => 'Federico',
+                'lastname' => 'Frascà',
+                'vote' => [7,8,7,9,10,8,6,10,8,6]
+     
+             ],
+
+              [
+                'name' => 'Pablo',
+                'lastname' => 'Silva',
+                'vote' => [7,8,10,9,9,8,6,10,8,6]
+     
+             ],
+  ];
+ 
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
