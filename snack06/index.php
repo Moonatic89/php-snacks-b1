52c2b21f348bcbf39bc3d4931cefa044a296d41c
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-02 15:24:53 +0100

    Snack06 init

--- snack06/index.php
+++ snack06/index.php
@@ -1,3 +1,33 @@
+<?php
+ 
+//Dall'array, stampa gli insegnanti in un rettangolo grigio e i PM in uno verde
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
