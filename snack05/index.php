b2a4d3dd703bbb63fc457db03a253e89629d86c0
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-02 15:12:32 +0100

    Snack 05 - Making structure

--- snack05/index.php
+++ snack05/index.php
@@ -1,3 +1,14 @@
+<?php
+
+//- Prendi un paragrafo lungo e dividilo in paragrafi più piccoli, un paragrafo dopo ogni punto.
+
+$sentence = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut fugiat doloribus odio, voluptatum quas, 
+laudantium dolorem iusto unde ad eum voluptas? Corporis, ratione qui. Ex nemo voluptas, non accusamus 
+consequatur praesentium atque iusto fugiat vel totam quis exercitationem esse dolorum?";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +19,23 @@
 </head>
 <body>
     
+<h1>SNACK 05 - Paragraph divider</h1>
+
+<p>
+    <h2>Original sentence:</h2>
+<?php 
+echo $sentence;
+?>
+
+    <h2>Modified sentence:</h2>
+    
+<?php 
+echo $sentence;
+?>
+
+
+</p>
+
+
 </body>
 </html>
