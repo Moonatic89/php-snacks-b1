7f9352f05e418ee883791759f0f04581a278833a
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-02 12:13:21 +0100

    Snack 2 push

--- snack02/index.php
+++ snack02/index.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>snack2
-    </title>
+    <title>snack2</title>
 </head>
 
 <body>
@@ -17,13 +16,31 @@
 -Verifica che età sia un numero
 
 Se è tutto corretto, stampa Accesso Riuscito, altrimenti Accesso Negato
-
-
-
-
  -->
+ 
+<?php
+ $name= $_GET["name"];
+ $mail= $_GET["text"];
+ $age= $_GET["age"];
+?>
+
+<h1>TITOLO</h1>
+
+<div class="formgroup">
+
+    <label for="name">Inserisci Nome</label>
+    <input type="text" id="name">
+
+    <label for="name">Inserisci Email</label>
+    <input type="text" id="text">
+
+    <label for="name">Inserisci Età</label>
+    <input type="text" id="age">
 
 
+</div>
+
+<div class="result"></div>
 
 </body>
 
